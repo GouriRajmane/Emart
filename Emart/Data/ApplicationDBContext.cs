e6ad221d8732f3ca799faeb73e80f51592251f21
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-30 13:47:21 +0530

    Added the Data folder with ApplicationDBContext data file

---
+++ Emart/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Emart.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
+
